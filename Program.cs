7eacf81836a51e8c1bfd341f591dbac9e5b37998
Максим Симан <simanmak@yandex.ru>
2024-06-01 14:52:09 +0300

    комит

--- Program.cs
+++ Program.cs
@@ -1,42 +1,68 @@
-﻿int a = 4;
-int b = 5;
-if (a > 1)
+﻿//int a = 4;
+//int b = 5;
+//if (a > 1)
+//{
+//    if (b > 2)
+//    {
+//        int sum = a + b;
+//        Console.WriteLine(sum);
+//    }
+//}
+//if (a > 1)
+//{
+//    if (b > 2)
+//    {
+//        int sum = a + b;
+//        Console.WriteLine(sum);
+//    }
+//}
+//if (a > 1)
+//{
+//    if (b > 2)
+//    {
+//        int sum = a + b;
+//        Console.WriteLine(sum);
+//    }
+//}
+//foreach (char c in "Tom")
+//{
+//    Console.Write(c);
+//}
+//if (a > 1)
+//{
+//    if (b > 2)
+//    {
+//        int sum = a + b;
+//        Console.WriteLine(sum);
+//    }
+//}
+//foreach (char c in "Tom")
+//{
+//    Console.Write(c);
+//}
+
+
+//string r = "Row";
+//string h = "Low";
+//if (r == h)
+//{
+//    Console.Write("одинаковые значения");
+//}
+
+
+
+
+
+
+
+
+string a = "Hello";
+string b = "Hello";
+if (a == b)
 {
-    if (b > 2)
-    {
-        int sum = a + b;
-        Console.WriteLine(sum);
-    }
+    Console.WriteLine("значения одинаковые");
 }
-if (a > 1)
+else
 {
-    if (b > 2)
-    {
-        int sum = a + b;
-        Console.WriteLine(sum);
-    }
-}
-if (a > 1)
-{
-    if (b > 2)
-    {
-        int sum = a + b;
-        Console.WriteLine(sum);
-    }
-}
-foreach (char c in "Tom")
-{
-    Console.Write(c);
-}
-if (a > 1)
-{
-    if (b > 2)
-    {
-        int sum = a + b;
-        Console.WriteLine(sum);
-    }
-}
-foreach (char c in "Tom")
-{
-    Console.Write(c);
+    Console.WriteLine($"{a} {b}");
 }
