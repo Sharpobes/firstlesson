66f2cf16860c3ccd7d8d3d1fded83f1f83d1a3eb
Максим Симан <simanmak@yandex.ru>
2024-06-01 14:18:40 +0300

    фывфыв

--- Program.cs
+++ Program.cs
@@ -28,3 +28,15 @@ foreach (char c in "Tom")
 {
     Console.Write(c);
 }
+if (a > 1)
+{
+    if (b > 2)
+    {
+        int sum = a + b;
+        Console.WriteLine(sum);
+    }
+}
+foreach (char c in "Tom")
+{
+    Console.Write(c);
+}
