6e3f1e2289c15fae34391c27ff3fa668be3f5c7f
Максим Симан <simanmak@yandex.ru>
2024-06-01 14:14:33 +0300

    добавил еще два оператора if

--- Program.cs
+++ Program.cs
@@ -8,7 +8,22 @@ if (a > 1)
         Console.WriteLine(sum);
     }
 }
-
+if (a > 1)
+{
+    if (b > 2)
+    {
+        int sum = a + b;
+        Console.WriteLine(sum);
+    }
+}
+if (a > 1)
+{
+    if (b > 2)
+    {
+        int sum = a + b;
+        Console.WriteLine(sum);
+    }
+}
 foreach (char c in "Tom")
 {
     Console.Write(c);
