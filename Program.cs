17c4b1aeb0e022cd6d5f374cd6e8b56ae16fc6bd
max <max@DESKTOP-CK8ANNH>
2024-06-01 14:06:56 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 4;
+int b = 5;
+if (a > 1)
+{
+    if (b > 2)
+    {
+        int sum = a + b;
+        Console.WriteLine(sum);
+    }
+}
+
+foreach (char c in "Tom")
+{
+    Console.Write(c);
+}
